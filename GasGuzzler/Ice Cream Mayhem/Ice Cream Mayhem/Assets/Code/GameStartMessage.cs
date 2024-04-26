ab5bc12b54027651e53ec5f3d11547292dd81281
Seth Corbran <monkeybean1464@gmail.com>
2024-04-26 13:40:58 -0600

    Finished Mobile Game Build
    A submission of our Mobile Game build=.

---
+++ GasGuzzler/Ice Cream Mayhem/Ice Cream Mayhem/Assets/Code/GameStartMessage.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e4be8c9f49d3fc1614bc644ccb7882aabe3511c584d5c0fbca0d1aad198747e
+size 3442
---
+++ GasGuzzler/Ice Cream Mayhem/Ice Cream Mayhem/Assets/Code/OculusQuestControls.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b14d6ad8ca2e2a2c31d8fba622badfff75901c726d65d4efc508e11dbf61df1
+size 2363
